cb0d50ce16f2ffce082dd021ef2071c050a17e0c
Théo FERNANDO <theo.fernando@isen.yncrea.fr>
2025-03-11 17:55:27 +0100

    Style login screen

--- app/src/main/java/fr/isen/animalpark/screens/sign_in/SignInScreen.kt
+++ app/src/main/java/fr/isen/animalpark/screens/sign_in/SignInScreen.kt
@@ -3,19 +3,33 @@ package fr.isen.animalpark.screens.sign_in
 import android.content.Intent
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.firebase.auth.FirebaseAuth
 import fr.isen.animalpark.LoginActivity
 import fr.isen.animalpark.MainActivity
@@ -30,9 +44,31 @@ fun SignInScreen(auth: FirebaseAuth) {
     val context = LocalContext.current
 
 
-    Column(modifier = Modifier.padding(top = 46.dp)) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         OutlinedTextField(
             singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp, 4.dp)
+                .border(
+                    BorderStroke(width = 2.dp, color = Color.Black),
+                    shape = RoundedCornerShape(50)
+                ),
+            colors = TextFieldDefaults.colors(
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent,
+                errorContainerColor = Color.Transparent
+            ),
             value = email.value,
             onValueChange = {
                 email.value = it
@@ -41,6 +77,21 @@ fun SignInScreen(auth: FirebaseAuth) {
         )
         OutlinedTextField(
             singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp, 4.dp)
+                .border(
+                    BorderStroke(width = 2.dp, color = Color.Black),
+                    shape = RoundedCornerShape(50)
+                ),
+            colors = TextFieldDefaults.colors(
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent,
+                errorContainerColor = Color.Transparent
+            ),
             value = password.value,
             onValueChange = {
                 password.value = it
@@ -48,7 +99,15 @@ fun SignInScreen(auth: FirebaseAuth) {
             placeholder = { Text("Password") },
             visualTransformation = PasswordVisualTransformation()
         )
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp)
+        )
         Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp, 0.dp),
             onClick = {
                 if (email.value == "" || password.value == "") {
                     Toast.makeText(context, "Please fill in all fields", Toast.LENGTH_SHORT).show()
@@ -69,12 +128,17 @@ fun SignInScreen(auth: FirebaseAuth) {
                             // If sign in fails, display a message to the user.
                             Log.d("Login", "signInWithEmail:failure", task.exception)
                             //Toast login failed
-                            Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT)
+                                .show()
                         }
                     }
             },
         ) {
-            Text("Sign In")
+            Text(
+                text = "Sign In",
+                fontSize = 16.sp,
+                modifier = Modifier.padding(0.dp, 6.dp)
+            )
         }
         TextButton(
             onClick = {
@@ -83,7 +147,7 @@ fun SignInScreen(auth: FirebaseAuth) {
                 context.startActivity(intent)
             }
         ) {
-           Text("Create an account")
+            Text("Create an account")
         }
     }
 }
--- app/src/main/java/fr/isen/animalpark/screens/sign_up/SignUpScreen.kt
+++ app/src/main/java/fr/isen/animalpark/screens/sign_up/SignUpScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
